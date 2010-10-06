83fc48d17c933e30209b46c204042ef81c2d5da8
Matthew Landauer <matthew@openaustralia.org>
2010-10-06 16:40:14 +1100

    Super simple form that displays the values filled out in the form

--- civicrm-wordpress-contact-form.php
+++ civicrm-wordpress-contact-form.php
@@ -8,4 +8,38 @@ Version: 0.0.1
 Author URI: 
 */
 
+function civicrm_form_shortcode($attrs)
+{
+	if ($_POST) {
+		echo "<p>Values just submitted:</p>";
+		echo "<p>First Name: {$_POST["first_name"]}</p>";
+		echo "<p>Last Name: {$_POST["last_name"]}</p>";
+		echo "<p>Email: {$_POST["email"]}</p>";
+	}
+	else {
+?>
+	<form action="" method="post" accept-charset="utf-8" id="contact">
+	<p>
+		Your First Name: <br>
+		<input type="text" name="first_name" value="" id="first_name" />
+	</p>
+	
+	<p>
+		Your Last Name:<br>
+		<input type="text" name="last_name" value="" id="last_name" />
+	</p>
+	
+	<p>
+		Email Address:<br>
+		<input type="text" name="email" value="" id="email" />
+	</p>
+	
+	<input type="submit" value="contact" name="contact">
+</form>
+<?php
+	}
+}
+
+add_shortcode('civicrm', 'civicrm_form_shortcode');	
+
 ?>
