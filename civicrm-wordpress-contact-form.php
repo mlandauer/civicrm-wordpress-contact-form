a9c14499feafca51a87a17fb8e13acd91d4619ff
Matthew Landauer <matthew@openaustralia.org>
2010-10-06 15:41:21 +1100

    Just enough to tell Wordpress that this plugin exists

---
+++ civicrm-wordpress-contact-form.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: CiviCRM Contact Form
+Plugin URI: 
+Description: Easily embed a contact form anywhere and capture the entries straight into CiviCRM
+Author: Matthew Landauer - OpenAustralia Foundation
+Version: 0.0.1
+Author URI: 
+*/
+
+?>
